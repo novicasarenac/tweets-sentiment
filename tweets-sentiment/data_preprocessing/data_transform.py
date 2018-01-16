379c435d87f49d034acd766225205444f5631498
Predrag Njegovanovic <djaps94@gmail.com>
2018-01-16 23:14:49 +0100

    Transformation of data with emoticons removal and token stemming

---
+++ tweets-sentiment/data_preprocessing/data_transform.py
@@ -0,0 +1,38 @@
+from nltk.tokenize import TweetTokenizer
+from nltk.stem import PorterStemmer
+
+POSITIVE_EMOTICONS = set([':)', '(:', ':]', '=]', ':D', ';)', ':-)', ':-]',
+                          ':=]', ':-D', ':-))', '=)', ':-}', ':}', '=D'])
+
+NEGATIVE_EMOTICONS = set([':(', '):', '=(', ':O', '=O', ':\\', ':-X', ':-|',
+                          ':\'(', ':-\'(', ':[', ':-[', ':-(', ':@', ':P', ':-P'])
+
+POSITIVE_WORD = 'happy'
+NEGATIVE_WORD = 'sad'
+
+
+def init_tokenizer(preserve_case=False, strip_handles=False, reduce_len=True):
+    return TweetTokenizer(preserve_case, strip_handles, reduce_len)
+
+
+def transform_post(twitter_post):
+    tokenizer = init_tokenizer()
+    stemmer = PorterStemmer()
+    tokens = []
+    for token in tokenizer.tokenize(twitter_post):
+        tokens.append(stemming(stemmer, emoticon_transformation(token)))
+
+    return ' '.join(tokens)
+
+
+def stemming(stemmer, token):
+    return stemmer.stem(token)
+
+
+def emoticon_transformation(token):
+    if(token in POSITIVE_EMOTICONS):
+        return POSITIVE_WORD
+    elif(token in NEGATIVE_EMOTICONS):
+        return NEGATIVE_WORD
+
+    return token
