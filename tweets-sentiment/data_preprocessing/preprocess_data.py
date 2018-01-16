847890729ed1e0ef384fe56ec9b9a96b5119af72
SasaFerenc <sasa94sm2@gmail.com>
2018-01-16 21:32:12 +0100

    Preprocessing for loop separating sentiments and tweets.

--- tweets-sentiment/data_preprocessing/preprocess_data.py
+++ tweets-sentiment/data_preprocessing/preprocess_data.py
@@ -2,11 +2,34 @@ import csv
 
 path = '../../data/data.csv'
 
+processedData = '../../data/preprocessedData.csv'
+separator = 'Sentiment140,'
+
+def preprocessing(data):
+    with open(processedData, "w") as preprocessedData_file:
+
+        for index, x in enumerate(data):
+            if separator in x:
+                sentiment = x.split(',')[1]
+                tweet = x.split(separator)[1]
+                #TODO: pozvati funkcije, proslediti tweet
+
+                line = sentiment + separator + tweet
+                preprocessedData_file.write(line)
+
+    preprocessedData_file.close()
+
 def main():
     with open(path, 'rb') as csvfile:
         data = [next(csvfile) for x in range(100)]
-    for x in data:
-        print(bytes.decode(x))
+    preprocessing(data)
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
+
