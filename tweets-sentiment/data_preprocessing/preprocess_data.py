97532d774c347f9c4c49ac645e488f6927a6f10d
SasaFerenc <sasa94sm2@gmail.com>
2018-01-17 01:18:14 +0100

    Sentiment and tweets list created

--- tweets-sentiment/data_preprocessing/preprocess_data.py
+++ tweets-sentiment/data_preprocessing/preprocess_data.py
@@ -4,12 +4,26 @@ import data_transform as dt
 
 path = '../../data/data.csv'
 
+testData = '../../data/testData.csv'
 processedData = '../../data/preprocessedData.csv'
 separator = 'Sentiment140,'
 
-def preprocessing(data):
-    with open(processedData, "w") as preprocessedData_file:
+def vector_representation(fileName):
+    with open(fileName, 'r') as testFile:
+        data = [next(testFile) for x in range(19966)]
 
+    sentiments = []
+    tweets = []
+    for index, x in enumerate(data):
+        sentiment, tweet = x.split(separator)
+        sentiments.insert(index, sentiment)
+        tweets.insert(index, tweet)
+
+    return  list(map(int, sentiments)), tweets
+
+
+def preprocessing(data, fileName):
+    with open(fileName, "w") as preprocessedData_file:
         for index, x in enumerate(data):
             if separator in x:
                 sentiment = x.split(',')[1]
@@ -17,15 +31,18 @@ def preprocessing(data):
                 #TODO: pozvati funkcije, proslediti tweet
                 tweet =  dt.transform_post(cd.clear_data(tweet))
                 tweet = cd.remove_special_characters(tweet)
-                line = sentiment + separator + tweet + " ==== " + x + "\n"
+                line = sentiment + separator + tweet + "\n"
                 preprocessedData_file.write(line)
 
-    preprocessedData_file.close()
+        preprocessedData_file.close()
 
 def main():
-    with open(path, 'r') as csvfile:
-        data = [next(csvfile) for x in range(100)]
-    preprocessing(data)
+   # with open(path, 'r', encoding="utf8") as csvfile:
+   #     data = [next(csvfile) for x in range(35000)]
+   # preprocessing(data[0:20000], testData)
+   # preprocessing(data[20000:35000], processedData)
+   vector_representation(testData)
+
 
 if __name__ == "__main__":
     main()
