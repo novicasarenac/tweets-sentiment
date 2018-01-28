964b645f38e9a038c1f5c6d526d3d433be1647a5
Predrag Njegovanovic <djaps94@gmail.com>
2018-01-28 20:01:03 +0100

    Add lemmatization of twitter posts as preprocess step

--- tweets_sentiment/data_preprocessing/data_transform.py
+++ tweets_sentiment/data_preprocessing/data_transform.py
@@ -1,5 +1,7 @@
+import nltk
 import nltk.tokenize
-from nltk.stem import PorterStemmer
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
 
 POSITIVE_EMOTICONS = set([':)', '(:', ':]', '=]', ':D', ';)', ':-)', ':-]',
                           ':=]', ':-D', ':-))', '=)', ':-}', ':}', '=D'])
@@ -17,16 +19,37 @@ def init_tokenizer(preserve_case=False, strip_handles=False, reduce_len=True):
 
 def transform_post(twitter_post):
     tokenizer = init_tokenizer()
-    stemmer = PorterStemmer()
     tokens = []
     for token in tokenizer.tokenize(twitter_post):
-        tokens.append(stemming(stemmer, emoticon_transformation(token)))
+        tokens.append(emoticon_transformation(token))
 
     return ' '.join(tokens)
 
 
-def stemming(stemmer, token):
-    return stemmer.stem(token)
+def lemmatization(pos_tag_sentence):
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_words = []
+    for pos_tuple in pos_tag_sentence:
+        lemmatized_words.append(lemmatizer.lemmatize(pos_tuple[0], transform_tag(pos_tuple[1])))
+
+    return ' '.join(lemmatized_words)
+
+
+def pos_tagging(tokenized_post):
+    return nltk.pos_tag(tokenized_post)
+
+
+def transform_tag(tag):
+    if tag.startswith('J'):
+        return wordnet.ADJ
+    elif tag.startswith('V'):
+        return wordnet.VERB
+    elif tag.startswith('N'):
+        return wordnet.NOUN
+    elif tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
 
 
 def emoticon_transformation(token):
@@ -36,3 +59,11 @@ def emoticon_transformation(token):
         return NEGATIVE_WORD
 
     return token
+
+
+if __name__ == "__main__":
+    sentence = "going"
+    tokenized_sentence = init_tokenizer().tokenize(sentence)
+    pos_tagged = pos_tagging(tokenized_sentence)
+    print(pos_tagged)
+    print(lemmatization(pos_tagged))
