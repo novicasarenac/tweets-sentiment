4e02203836d4b909ac3b3efc431c798090f1edd6
Predrag Njegovanovic <djaps94@gmail.com>
2018-02-12 22:50:01 +0100

    Move training and evaluation to separate file

--- tweets_sentiment/models/naive_bayes_classifier.py
+++ tweets_sentiment/models/naive_bayes_classifier.py
@@ -1,38 +1,17 @@
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import classification_report
 from os import path
-import words_embedding as we
-import pandas as pd
+from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
+
+import pandas as pd
+import words_embedding as we
+import train_and_eval as te
 
 full_path = path.dirname(path.abspath(__file__ + "/../"))
 dataset = path.join(full_path, 'data/preprocessed_dataset.csv')
 
 
-def train(labels, tweets):
-    bag_of_words, vectorizer = we.make_bag_of_words(tweets)
-    classifier = MultinomialNB()
-    classifier.fit(bag_of_words.toarray(), labels)
-    return classifier, vectorizer
-
-
-def compute_accuracy(classifier, vectorizer, tweets, labels):
-    transformed_tweets = vectorizer.transform(tweets).toarray()
-    prediction = classifier.predict(transformed_tweets)
-    # precision, recall, f1 score
-    target_names = ['negative', 'positive']
-    report = classification_report(labels, prediction, target_names = target_names)
-    print(report)
-    # classifier score
-    classifier_score = classifier.score(transformed_tweets, labels)
-    print('Score: ' + str(classifier_score))
-
-
-def evaluate(classifier, vectorizer, X_train, X_test, y_train, y_test):
-    print('Training set accuracy: ')
-    compute_accuracy(classifier, vectorizer, X_train, y_train)
-    print('Test set accuracy: ')
-    compute_accuracy(classifier, vectorizer, X_test, y_test)
+def init_naive_bayes():
+    return MultinomialNB()
 
 
 def get_sentiment(classifier, vectorizer, tweet):
@@ -49,8 +28,9 @@ def read_data():
 
 
 if __name__ == '__main__':
+    nb = init_naive_bayes()
     labels, tweets = read_data()
-    X_train, X_test, y_train, y_test = train_test_split(tweets, labels, test_size = 0.33)
-
-    classifier, vectorizer = train(y_train, X_train)
-    evaluate(classifier, vectorizer, X_train, X_test, y_train, y_test)
+    X_train, X_test, y_train, y_test = train_test_split(tweets, labels, test_size=0.33)
+    feature_vector, vectorizer = we.make_bag_of_words(tweets)
+    classifier = te.train(nb, X_train, y_train)
+    te.evaluate(classifier, vectorizer, X_train, X_test, y_train, y_test)
