d7ed2e89e98c0615a0dcf7f935563f984f65c1d1
novicasarenac <nolesarenac@yahoo.com>
2018-02-20 00:00:56 +0100

    transform new dataset

---
+++ tweets_sentiment/data_preprocessing/transform_dataset.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from os import path
+
+
+FULL_PATH = path.dirname(path.abspath(__file__ + "/../"))
+FULL_DATASET = path.join(FULL_PATH, 'data/text_emotion.csv')
+DATASET_DESTINATION = path.join(FULL_PATH, 'data/new_data.csv')
+
+
+SENTIMENTS = {
+    'positive': [
+        'love',
+        'surprise',
+        'fun',
+        'relief'
+        'happiness',
+        'enthusiasm',
+    ],
+    'negative': [
+        'sadness',
+        'worry',
+        'hate',
+        'boredom',
+        'anger'
+    ],
+    'unlabelled': [
+        'empty',
+        'neutral'
+    ]
+}
+
+
+def transform_dataset():
+    data = pd.read_csv(FULL_DATASET)
+    new_data = pd.DataFrame(columns = ['sentiment', 'tweet'])
+
+    for index, tweet in data.iterrows():
+        if tweet['sentiment'] not in SENTIMENTS['unlabelled']:
+            new_data.loc[len(new_data)] = [1 if tweet['sentiment'] in SENTIMENTS['positive'] else 0, tweet['content']]
+    new_data.to_csv(DATASET_DESTINATION)
+
+
+if __name__ == '__main__':
+    transform_dataset()
