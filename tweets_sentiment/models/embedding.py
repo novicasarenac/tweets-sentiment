1b735d837cf257a959c1f6189a67159f3be442ab
novicasarenac <nolesarenac@yahoo.com>
2018-02-25 23:07:07 +0100

    bigrams added

--- tweets_sentiment/models/embedding.py
+++ tweets_sentiment/models/embedding.py
@@ -2,8 +2,9 @@ from sklearn.feature_extraction.text import CountVectorizer
 
 
 def make_bag_of_words(tweets):
-    vectorizer = CountVectorizer()
+    vectorizer = CountVectorizer(ngram_range=(1,2), max_features=30000)
     bag_of_words = vectorizer.fit_transform(tweets)
+    print('===> Features number:%s'%len(vectorizer.vocabulary_))
     return bag_of_words, vectorizer
 
 
