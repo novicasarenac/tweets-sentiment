18bb4078d739d0ffc59cbc3dfdb1436f215ec3bd
Predrag Njegovanovic <djaps94@gmail.com>
2018-04-13 18:56:40 +0200

    Train ml models on dataset partitions

--- tweets_sentiment/models/train_and_eval.py
+++ tweets_sentiment/models/train_and_eval.py
@@ -1,24 +1,38 @@
 import json
 import pandas as pd
+import numpy as np
 
 from os import path
+from numpy.random import shuffle
 from pipeline import bag_of_words
 from xgboost import XGBClassifier
+from sklearn.utils import shuffle
 from sklearn.svm import LinearSVC
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
+from tweets_sentiment.preprocessing.preprocess import read_corpus_dataset
 from tweets_sentiment.preprocessing.constants import FULL_PATH
 from tweets_sentiment.preprocessing.constants import PREPROCESSED_DATASET
 
 
-def read_data():
-    data = pd.read_csv(PREPROCESSED_DATASET)
-    labels = data['sentiment']
-    tweets = data['tweet'].values.astype('U')
+MIN_PARTITION = 20000
+MAX_PARTITION = 1500000
 
-    return labels, tweets
+NB_PARAMETERS = "data/nb_parameters.json"
+LR_PARAMETERS = "data/lr_parameters.json"
+SVM_PARAMETERS = "data/svm_parameters.json"
+XGB_PARAMETERS = "data/xgb_parameters.json"
+
+
+def read_partition(dataset_path, size):
+    data = pd.read_csv(dataset_path, error_bad_lines=False)
+    data = shuffle(data)
+    partition = data.iloc[:size]
+    tweets = partition['SentimentText'].values.astype('U')
+    labels = partition['Sentiment']
+    return tweets, labels
 
 
 def read_params(clf_params_name):
@@ -48,42 +62,48 @@ def evaluate(pipeline, X_train, X_test, y_train, y_test):
     compute_accuracy(pipeline, X_test, y_test)
 
 
-def load_pipelines(tf_idf):
-    print("===> Loading all pipelines...")
+def load_pipeline(model, params_path=None, tf_idf=False, params=None):
+    pipeline = bag_of_words(model, tf_idf)
+    if params_path:
+        pipeline.set_params(**read_params(params_path))
+    elif params:
+        pipeline.set_params(params)
 
-    nb_pipeline = bag_of_words(MultinomialNB(), tf_idf)
-    lr_pipeline = bag_of_words(LogisticRegression(), tf_idf)
-    svm_pipeline = bag_of_words(LinearSVC(), tf_idf)
-    xgb_pipeline = bag_of_words(XGBClassifier(), tf_idf)
+    return pipeline
 
-    print("===> Setting parameters...")
-    nb_pipeline.set_params(**read_params("data/nb_parameters.json"))
-    lr_pipeline.set_params(**read_params("data/lr_parameters.json"))
-    svm_pipeline.set_params(**read_params("data/svm_parameters.json"))
-    xgb_pipeline.set_params(**read_params("data/xgb_parameters.json"))
 
-    pipelines = {'Naive Bayes': nb_pipeline,
-                 'Logistic Regression': lr_pipeline,
-                 'Linear SVM': svm_pipeline,
-                 'XGBooster': xgb_pipeline,
-                 }
-
+def set_pipelines(pipelines, key, value):
+    pipelines = {key: value}
     return pipelines
+
+
+def partition_corpus(min_examples, max_examples, n_partition=5):
+    return np.geomspace(min_examples, max_examples, num=n_partition)
 
 
 if __name__ == "__main__":
     # load classifier pipelines with or without Tfidf transformer
-    pipelines = load_pipelines(False)
-    labels, tweets = read_data()
-    X_train, X_test, y_train, y_test = train_test_split(tweets,
-                                                        labels,
-                                                        test_size=0.2)
-    print("===> Fitting pipelines:")
-    for key, value in pipelines.items():
-        value.fit(X_train, y_train)
+    pipelines = {}
+    print('===> Loading pipelines')
+    pipeline = load_pipeline(LogisticRegression(n_jobs=-1), LR_PARAMETERS)
+    pipes = set_pipelines(pipelines, 'Logistic Regression', pipeline)
 
-    print("===> Evaluating pipelines:")
-    for key, value in pipelines.items():
-        print("\n")
-        print("{:>15}:".format(key))
-        evaluate(value, X_train, X_test, y_train, y_test)
+    partition_sizes = partition_corpus(MIN_PARTITION, MAX_PARTITION)
+    partition_sizes = [int(round(x)) for x in partition_sizes]
+
+    for index, partition_size in enumerate(partition_sizes):
+        print('===> Iteration {}. Partition size: {}'.format(index + 1,
+                                                             partition_size))
+        tweets, labels = read_partition(PREPROCESSED_DATASET, partition_size)
+        X_train, X_test, y_train, y_test = train_test_split(tweets,
+                                                            labels,
+                                                            test_size=0.2)
+        print("===> Fitting pipelines:")
+        for key, value in pipes.items():
+            value.fit(X_train, y_train)
+
+        print("===> Evaluating pipelines:")
+        for key, value in pipes.items():
+            print("\n")
+            print("{:>15}:".format(key))
+            evaluate(value, X_train, X_test, y_train, y_test)
